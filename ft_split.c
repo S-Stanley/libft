4c2458f82d288b2eb0f3973b86382906ebfa69d1
Stanley SERBIN <stanleyserbin@gmail.com>
2020-11-28 21:51:54 +0100

    ALL GOOD

--- ft_split.c
+++ ft_split.c
@@ -15,98 +15,71 @@
 int		ft_count(const char *str, char c)
 {
 	int		i;
-	int		n;
+	int		count;
+	int		size;
 
 	i = 0;
-	n = 0;
-	while (str[i])
+	count = 0;
+	size = (int)ft_strlen((char *)str);
+	while (i < size && str[i])
 	{
-		if (str[i] == c)
-			n++;
-		i++;
+		while (i < size && str[i] != c)
+			i++;
+		count++;
+		while (i < size && str[i] == c)
+			i++;
 	}
-	return (n + 1);
+	return (count + 1);
 }
 
-int		ft_multi_fct(const char *str, char c, int start, int mode)
-{
-	int count;
-
-	if (mode == 0)
-	{
-		count = 0;
-		while (str[start] && str[start] != c)
-		{
-			start++;
-			count++;
-		}
-		return (count);
-	}
-	else
-	{
-		count = 0;
-		while (str[count])
-			count++;
-		return (count);
-	}
-}
-
-char	*ft_split_the_tea(const char *str, char c, int start)
+char	*ft_new(char *str, int start, int end)
 {
 	char	*res;
 	int		i;
 
 	i = 0;
-	res = malloc(sizeof(char) * (ft_multi_fct(str, c, start, 0) + 1));
-	while (str[start] != c)
-	{
-		res[i] = str[start];
-		start++;
-		i++;
-	}
+	res = malloc(sizeof(char) * (end - start + 1));
+	if (!res)
+		return (NULL);
+	while (start != end)
+		res[i++] = str[start++];
 	res[i] = '\0';
 	return (res);
 }
 
-char	**ft_split2(char const *s, char c, int x, int i)
+char	*ft_split_2(char *str, char c, int i)
 {
-	char	**res;
-	int		n;
+	int start;
 
-	n = 0;
-	if (!(res = malloc(sizeof(char *) * ft_count(s, c))))
-		return (NULL);
-	res[x] = malloc(sizeof(char) * (ft_multi_fct(s, 'c', 0, 1) + 1));
-	while (s[i])
-	{
-		if (s[i] != c)
-			res[x][n++] = s[i++];
-		else
-		{
-			res[x][n] = '\0';
-			res[++x] = malloc(sizeof(char) * (ft_multi_fct(s, 'c', 0, 1) + 1));
-			i++;
-			n = 0;
-			while (s[i] == c)
-				i++;
-		}
-	}
-	if (s[ft_strlen((char *)s) - 1] != c)
-		res[x++][n] = '\0';
-	res[x] = NULL;
-	return (res);
+	start = i;
+	while (str[i] != c && str[i])
+		i++;
+	return (ft_new(str, start, i));
 }
 
-char	**ft_split(char const *s, char c)
+char	**ft_split(const char *str, char c)
 {
 	int		i;
 	int		x;
+	char	**res;
+	int		start;
+	int		size;
 
-	if (s == NULL)
+	if (!(res = malloc(sizeof(char *) * ft_count(str, c))) || !str)
 		return (NULL);
 	i = 0;
-	x = 0;
-	while (s[i] == c)
-		i++;
-	return (ft_split2(s, c, x, i));
+	x = -1;
+	size = (int)ft_strlen((char *)str);
+	while (i < size)
+	{
+		start = i;
+		while (i < size && str[i] != c)
+			i++;
+		if (start != 0 || str[start] != c)
+			res[++x] = ft_new((char *)str, start, i);
+		while (i < size && str[i] == c)
+			i++;
+	}
+	res[++x] = NULL;
+	return (res);
 }
